86137ba83277d04dd48dfc6272091c16ef6e0373
walter <walter@trueplus.vn>
2017-04-06 18:37:51 +0700

    walter - product cloning progressing

--- sources/product_cloning.py
+++ sources/product_cloning.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
+import os, time, sys
+from datetime import datetime
