89be02f9d46421ed1bea1900060313e81688b055
walter <walter@trueplus.vn>
2017-04-06 18:13:41 +0700

    walter - category cloning done

--- sources/category_cloning.py
+++ sources/category_cloning.py
@@ -27,11 +27,46 @@ def SeriesClone():
 
 def CatClone():
 
+    driver.get('http://jokerface.vn/collections/all')
+
+    main_block = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'main_menu'))
+    )
+    print str(datetime.now()) + ' main block found.'
+
+    main_menu = main_block.find_elements_by_tag_name('a')
+    print str(datetime.now()) + ' tag a found.'
+    for elm_main_menu in main_menu:
+        try:
+            attr_title = elm_main_menu.get_attribute('title')
+            print attr_title
+        except NoSuchAttributeException:
+            print '---' + elm_main_menu.get_attribute('innerHTML')
+
+
+def SubCatClone():
+
+    driver.get('http://jokerface.vn/collections/all')
+
+    main_block = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'main_menu'))
+    )
+    print str(datetime.now()) + ' main block found.'
+
+    main_class = main_block.find_elements_by_class_name('sub_menu')
+    print str(datetime.now()) + ' sub menu found.'
+    for elm_main_class in main_class:
+        elm_a = elm_main_class.find_elements_by_tag_name('a')
+        for elm in elm_a:
+            print elm.get_attribute('innerHTML')
+
 
 if __name__ == '__main__':
 
     driver = webdriver.Chrome()
 
     # SeriesClone()
+    # CatClone()
+    SubCatClone()
 
     os.system('pkill chromedriver')
