ec6993c48c88faa7cfba7097c9f8603e19d50204
do duc huy <walter@trueplus.vn>
2017-05-06 12:52:43 +0700

    modified product information and interface

---
+++ customize/website_product_modified/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Product Additional Info',
+    'version': '1.0',
+    'category': 'Tools',
+    'description': """
+Product Modified
+=========================================================================
+    """,
+    'depends': ['base', 'web', 'website_sale'],
+    'data': [
+        
+    ],
+    'application': True,
+}
