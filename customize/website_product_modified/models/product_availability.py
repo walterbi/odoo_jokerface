b0c9911c0587d731d699dda1625fea19e399ff2c
do duc huy <walter@trueplus.vn>
2017-05-09 17:33:54 +0700

    walter - modified product sorting

--- customize/website_product_modified/models/product_availability.py
+++ customize/website_product_modified/models/product_availability.py
@@ -9,11 +9,9 @@ class ProductAvailability(models.Model):
 
     availability = fields.Selection(selection_add=[('out_of_stock', 'Out of Stock')])
 
-    @api.model
+    @api.multi
     def default_availability(self):
-
-        stock_product_change = self.env['stock.change.product.qty'].search([(
-            'product_id', '=', self.product_id,
-        )])
-        print stock_product_change
-        print "break point here"
+        stock_product_change = self.env['stock.change.product.qty'].search([('product_id', '=', self.id)])
+        on_hand = len(stock_product_change)
+        if on_hand == 0:
+            self.availability = 'out_of_stock'
